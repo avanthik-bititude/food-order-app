09ab1f40c00566cad211648a1e8222aa75765530
avanthik <avanthik@bititude.com>
2024-11-05 10:32:30 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,35 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isCheckoutOpen, setIsCheckoutOpen] = useState(false);
 
   const handleIsCartOpen = () => {
+    setIsCheckoutOpen(false);
     setIsCartOpen((prev) => !prev);
+  };
+
+  const handleIsCheckoutOpen = () => {
+    setIsCartOpen(false);
+    setIsCheckoutOpen((prev) => !prev);
   };
 
   return (
     <>
       <Navbar onClickCart={handleIsCartOpen} />
-      {isCartOpen ? <Cart onClickCart={handleIsCartOpen} /> : <Products />}
+      {isCartOpen && !isCheckoutOpen ? (
+        <Cart
+          onClickCart={handleIsCartOpen}
+          onClickCheckout={handleIsCheckoutOpen}
+        />
+      ) : !isCartOpen && isCheckoutOpen ? (
+        <Checkout onClickBack={handleIsCartOpen} />
+      ) : (
+        <Products />
+      )}
     </>
   );
 }
